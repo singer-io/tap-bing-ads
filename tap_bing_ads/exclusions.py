3f505617ea317cb19fa4a9243d25276e44effbac
Kyle Allan <kyle@stitchdata.com>
2018-10-03 14:31:53 -0400

    support fieldExclusions metadata for v12

---
+++ tap_bing_ads/exclusions.py
@@ -0,0 +1,93 @@
+EXCLUSIONS = {
+    'AccountPerformanceReport': {
+        'Attributes': [
+            'BidMatchType',
+            'DeviceOS',
+            'TopVsOther'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'ExactMatchImpressionSharePercent',
+            'ImpressionLostToAdRelevancePercent',
+            'ImpressionLostToBidPercent',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToExpectedCtrPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent'
+        ]
+    },
+    'AdGroupPerformanceReport': {
+        'Attributes': [
+            'BidMatchType',
+            'DeviceOS',
+            'TopVsOther'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'ExactMatchImpressionSharePercent',
+            'ImpressionLostToAdRelevancePercent',
+            'ImpressionLostToBidPercent',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToExpectedCtrPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent'
+        ]
+    },
+    'CampaignPerformanceReport': {
+        'Attributes': [
+            'BidMatchType',
+            'DeviceOS',
+            'TopVsOther',
+            'BudgetAssociationStatus',
+            'BudgetName',
+            'BudgetStatus'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'ExactMatchImpressionSharePercent',
+            'ImpressionLostToAdRelevancePercent',
+            'ImpressionLostToBidPercent',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToExpectedCtrPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent'
+        ]
+    },
+    'ProductDimensionPerformanceReport': {
+        'Attributes': [
+            'AdDistribution',
+            'AdId',
+            'AdStatus',
+            'ClickType',
+            'Language',
+            'LocalStoreCode',
+            'Network',
+            'TopVsOther'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'BenchmarkBid',
+            'BenchmarkCtr',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent'
+        ]
+    },
+    'ProductPartitionPerformanceReport': {
+        'Attributes': [
+            'AdDistribution',
+            'AdId',
+            'AdStatus',
+            'BidMatchType',
+            'ClickType',
+            'DeliveredMatchType',
+            'Language',
+            'LocalStoreCode',
+            'Network',
+            'TopVsOther'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'BenchmarkBid',
+            'BenchmarkCtr',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent'
+        ]
+    }
+}
