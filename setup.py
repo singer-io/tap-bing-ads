12ac81521f87522b347cf70cc628f4147718347e
Kyle Allan <KAllan357@gmail.com>
2020-04-27 11:18:47 -0400

    bump to 2.0.11 (#54)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tap-bing-ads',
-    version="2.0.10",
+    version="2.0.11",
     description='Singer.io tap for extracting data from the Bing Ads API',
     author='Stitch',
     url='http://singer.io',
