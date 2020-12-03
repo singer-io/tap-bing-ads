b9288c693cea0092c519a902f2934f390f0d71e7
Kyle Speer <kspeer@stitchdata.com>
2020-12-03 19:23:32 +0000

    recover discovery test

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -1,3 +1,89 @@
+"""
+Test tap discovery
+"""
+import re
+
+from tap_tester import menagerie, connections, runner
+
+from base import BingAdsBaseTest
+
+
+class DiscoveryTest(BingAdsBaseTest):
+    """ Test the tap discovery """
+
+    @staticmethod
+    def name():
+        return "tap_tester_bing_ads_discovery_test"
+
+    def test_run(self):
+        """
+        Verify that discover creates the appropriate catalog, schema, metadata, etc.
+        • Verify number of actual streams discovered match expected
+        • Verify the stream names discovered were what we expect
+        • Verify stream names follow naming convention
+          streams should only have lowercase alphas and underscores
+        • verify there is only 1 top level breadcrumb
+        • verify replication key(s)
+        • verify primary key(s)
+        • verify that if there is a replication key we are doing INCREMENTAL otherwise FULL
+        • verify the actual replication matches our expected replication method
+        • verify that primary, replication and foreign keys
+          are given the inclusion of automatic (metadata and annotated schema).
+        • verify that all other fields have inclusion of available (metadata and schema)
+        """
+        streams_to_test = self.expected_streams()
+
+        conn_id = self.create_connection()
+
+        # Verify number of actual streams discovered match expected
+        catalogs = menagerie.get_catalogs(conn_id)
+        found_catalogs = [catalog for catalog in catalogs
+                          if catalog.get('tap_stream_id') in streams_to_test]
+
+        self.assertGreater(len(found_catalogs), 0,
+                           msg="unable to locate schemas for connection {}".format(conn_id))
+        self.assertEqual(len(found_catalogs),
+                         len(streams_to_test),
+                         msg="Expected {} streams, actual was {} for connection {}, "
+                             "actual {}".format(
+                                 len(streams_to_test),
+                                 len(found_catalogs),
+                                 found_catalogs,
+                                 conn_id))
+
+        # Verify the stream names discovered were what we expect
+        found_catalog_names = {c['tap_stream_id'] for c in found_catalogs}
+        self.assertEqual(set(streams_to_test),
+                         set(found_catalog_names),
+                         msg="Expected streams don't match actual streams")
+
+        # Verify stream names follow naming convention
+        # streams should only have lowercase alphas and underscores
+        self.assertTrue(all([re.fullmatch(r"[a-z_]+", name) for name in found_catalog_names]),
+                        msg="One or more streams don't follow standard naming")
+
+        for stream in streams_to_test:
+            with self.subTest(stream=stream):
+                catalog = next(iter([catalog for catalog in found_catalogs
+                                     if catalog["stream_name"] == stream]))
+                assert catalog  # based on previous tests this should always be found
+
+                schema_and_metadata = menagerie.get_annotated_schema(conn_id, catalog['stream_id'])
+                metadata = schema_and_metadata["metadata"]
+                schema = schema_and_metadata["annotated-schema"]
+
+                # verify the stream level properties are as expected
+                # BUG | https://stitchdata.atlassian.net/browse/SRCE-4315
+                # # verify there is only 1 top level breadcrumb
+                # stream_properties = [item for item in metadata if item.get("breadcrumb") == []]
+                # self.assertTrue(len(stream_properties) == 1,
+                #                 msg="There is NOT only one top level breadcrumb for {}".format(stream) + \
+                #                 "\nstream_properties | {}".format(stream_properties))
+
+                # # verify replication key(s)
+                # self.assertEqual(
+                #     set(stream_properties[0].get(
+                #         "metadata", {self.REPLICATION_KEYS: []}).get(self.REPLICATION_KEYS, [])),
                 #     self.expected_replication_keys()[stream],
                 #     msg="expected replication key {} but actual is {}".format(
                 #         self.expected_replication_keys()[stream],
@@ -54,7 +140,7 @@
                 # actual_automatic_fields = {key for key, value in schema["properties"].items()
                 #                            if value.get("inclusion") == "automatic"}
                 # self.assertEqual(expected_automatic_fields, actual_automatic_fields)
-                         
+
 
                 # # verify that all other fields have inclusion of available
                 # # This assumes there are no unsupported fields for SaaS sources
