9c8dfde4c69fefe6ab104d708d09d5b054c4c541
atttiwari <atul.tiwari@qlik.com>
2026-07-22 15:08:10 +0530

    Fixing tap failure in circlrci

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         'arrow==0.17.0',
         # Seems that suds-community is now the reference for 13.0.11.1 so we can install it now with the removal of use_2to3
         # https://github.com/BingAds/BingAds-Python-SDK/pull/192
-        'bingads==13.0.11.1',
+        'bingads==13.0.28',
         'requests==2.34.2',
         'singer-python==6.8.0',
         'backoff==2.2.1',
