7a7a47611a9bad5b12cc14fc3b3f544de1989f34
Dylan Sprayberry <dylan.sprayberry@qlik.com>
2026-07-27 14:24:25 +0000

    Use env vars instead of hardcoding test account info

--- tests/base_new_framework.py
+++ tests/base_new_framework.py
@@ -41,8 +41,8 @@ class BingAdsBaseTest(BaseCase):
         """Configuration properties required for the tap."""
         return_value = {
             'start_date': self.start_date,
-            'customer_id': '163875182',
-            'account_ids': '163078754,140168565,71086605',
+            'customer_id': os.getenv('TAP_BING_ADS_CUSTOMER_ID'),
+            'account_ids': os.getenv('TAP_BING_ADS_ACCOUNT_IDS'),
             # 'conversion_window': '-15',  # advanced option
         }
         # cid=42183085 aid=71086605  uid=71069166 (RJMetrics)
