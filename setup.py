6496bc87b1e1bf5f23935e69b019a47b71f28932
Dylan Sprayberry <dylan.sprayberry@qlik.com>
2026-09-04 19:21:30 +0000

    Setuptools 84?

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
         # Seems that suds-community is now the reference for 13.0.11.1 so we can install it now with the removal of use_2to3
         # https://github.com/BingAds/BingAds-Python-SDK/pull/192
         'bingads==13.0.28',
+        'setuptools==84.0.0',
         'requests==2.34.2',
         'singer-python==6.8.0',
         'backoff==2.2.1',
